c16638e996a632c023098fa15d8d408a25e6c27a
kimrobbin <kimrobbinkf@gmail.com>
2025-04-24 11:50:21 +0200

    Update snail.py

--- snail.py
+++ snail.py
@@ -1,29 +1,28 @@
-# import pygame
+import pygame
 
-# class Snail(pygame.sprite.Sprite):
+class Snail(pygame.sprite.Sprite):
     
     
-#     def __init__(self, floor_top):
-#         super().__init__()
-#         self.surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
-#         self.rect = self.surface.get_rect(midbottom=(800, floor_top))
-#         self.respawn = self.surface.get_rect(midbottom=(800, floor_top))
-#         self.floor_top = floor_top
-#         self.move_speed = 4 
+    def __init__(self, floor_top):
+        super().__init__()
+        self.surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+        self.rect = self.surface.get_rect(midbottom=(800, floor_top))
+        self.respawn = self.surface.get_rect(midbottom=(800, floor_top))
+        self.floor_top = floor_top
+        self.move_speed = 4 
         
-#     def snail_move(self):
-#         self.rect.x -= self.move_speed
-#         if self.rect.right <= 0:
-#             self.rect.left = self.respawn.left
-        
-        
-#     def draw(self, screen):
-#         screen.blit(self.surface, self.rect)  
+    def snail_move(self):
+        self.rect.x -= self.move_speed
+        if self.rect.right <= 0:
+            self.rect.left = self.respawn.left
         
         
-#     def snail2_move(self):
-#         self.respawn.x -= self.move_speed
-#         if self.respawn.right <= 0:
-#             self.respawn.left = self.rect.left
+    def draw(self, screen):
+        screen.blit(self.surface, self.rect)  
+        
+        
+    def snail2_move(self):
+        self.respawn.x -= self.move_speed
+        if self.respawn.right <= 0:
+            self.respawn.left = self.rect.left
     
-print(0.1 + 0.3)
